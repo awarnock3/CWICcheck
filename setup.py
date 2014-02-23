bcf3fcbc971ff669ee91840fb0cabb43c0d449c0
Archie Warnock <warnock@awcubed.com>
2014-02-23 20:15:06 +0000

    Updating initial repo

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Describe the project here',
+    'description': 'A set of testing scripts to massage the CWIC OpenSearch interface',
     'author': 'Archie Warnock',
     'download_url': 'http://www.awcubed.com',
     'author_email': 'warnock@awcubed.com',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['CWICcheck'],
+    'packages': ['CWICcheck', 'requests', 'lxml', 'urlparse'],
     'scripts': [],
     'name': 'CWICcheck'
 }
