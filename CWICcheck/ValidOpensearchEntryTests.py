6e4b2d965562017b589d0d506b7b19642c3dc9bd
Archie Warnock <warnock@awcubed.com>
2014-02-24 20:19:19 +0000

    Added more individual tests

--- CWICcheck/ValidOpensearchEntryTests.py
+++ CWICcheck/ValidOpensearchEntryTests.py
@@ -5,15 +5,151 @@ nsmap = {None:           "http://www.w3.org/2005/Atom",
          "opensearch":   "http://a9.com/-/spec/opensearch/1.1/",
          "cwic":         "http://cwic.wgiss.ceos.org/opensearch/extensions/1.0/",
          "esipdiscover": "http://commons.esipfed.org/ns/discovery/1.2/",
+         "georss":       "http://www.georss.org/georss/10",
+         "dc":           "http://purl.org/dc/elements/1.1/",
          }
 
 def testEntryElement(root):
-    node = root.find("./atom:entry",namespaces=nsmap)
+    node = root.find("./atom:entry[1]",namespaces=nsmap)
     testResults = {}
     testName = "Found feed/entry"
     if (node is not None):
       testResults[testName] = True
     else:
       testResults[testName] = False
+    return testResults
 
+def testEntryTitleElement(root):
+    node = root.find("./atom:entry[1]/atom:title",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/title"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+      
+    if (node is not None):
+      testName = "feed/entry/title not empty"
+      if node.text:
+        testResults[testName] = True
+      else:
+        testResults[testName] = False
+    return testResults
+    
+def testEntryUpdatedElement(root):
+    node = root.find("./atom:entry[1]/atom:updated",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/updated"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+      
+    if (node is not None):
+      testName = "feed/entry/updated not empty"
+      if node.text:
+        testResults[testName] = True
+      else:
+        testResults[testName] = False
+    return testResults
+
+def testEntryAuthorElement(root):
+    node = root.find("./atom:entry[1]/atom:author",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/author/"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+    return testResults
+
+def testEntryAuthorNameElement(root):
+    node = root.find("./atom:entry[1]/atom:author/atom:name",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/author/name"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+      
+    if (node is not None):
+      testName = "feed/entry/author/name not empty"
+      if node.text:
+        testResults[testName] = True
+      else:
+        testResults[testName] = False
+    return testResults
+
+def testEntryAuthorEmailElement(root):
+    node = root.find("./atom:entry[1]/atom:author/atom:email",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/author/email"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+      
+    if (node is not None):
+      testName = "feed/entry/author/email not empty"
+      if node.text:
+        testResults[testName] = True
+      else:
+        testResults[testName] = False
+    return testResults
+
+def testEntryIdElement(root):
+    node = root.find("./atom:entry[1]/atom:id",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/id"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+      
+    if (node is not None):
+      testName = "feed/entry/id not empty"
+      if node.text:
+        testResults[testName] = True
+      else:
+        testResults[testName] = False
+    return testResults
+  
+def testEntryBoxElement(root):
+    node = root.find("./atom:entry[1]/georss:box",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/georss:box/"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+    return testResults
+
+def testEntryDateElement(root):
+    node = root.find("./atom:entry[1]/dc:date",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/dc:date/"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+    return testResults
+
+def testEntryViaLinkElement(root):
+    node = root.find("./atom:entry[1]/atom:link[@rel='via']",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/link[@rel='via']"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
+    return testResults
+
+def testEntryIconLinkElement(root):
+    node = root.find("./atom:entry[1]/atom:link[@rel='icon']",namespaces=nsmap)
+    testResults = {}
+    testName = "Found feed/entry/link[@rel='icon']"
+    if (node is not None):
+      testResults[testName] = True
+    else:
+      testResults[testName] = False
     return testResults
